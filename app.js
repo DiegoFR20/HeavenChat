807efee47921d952019f1dffde9cc664c129482e
DiegoFR <diegofreire8482@hotmail.com>
2020-05-21 11:55:51 -0300

    Limpar chat adicionado Lista de próximas atualizações Falta corrigir um bug de participantes para a 3ª ou maior sessão. Criar salas de chat.
    Atualizar lista de participantes de outra forma.

--- app.js
+++ app.js
@@ -9,24 +9,19 @@ var io = require('socket.io').listen(server);
 app.set('io', io);
 
 io.on('connection', function (socket) {
-    console.log('Usuário conectou');
 
     socket.on('msgParaServidor', function (data) {
         socket.on('disconnect', function () {
-            socket.emit(
-                'msgParaCliente',
-                { apelido: data.apelido, mensagem: 'acabou de sair do chat' });
 
             socket.broadcast.emit(
                 'msgParaCliente',
                 { apelido: data.apelido, mensagem: 'acabou de sair do chat' });
 
-            console.log('Usuário desconectou');
         });
 
         socket.emit(
             'msgParaCliente',
-            { apelido: data.apelido, mensagem: data.mensagem }
+            { apelido: 'Você', mensagem: data.mensagem }
         );
 
         socket.broadcast.emit(
